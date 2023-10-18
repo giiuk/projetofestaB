96d889aed861a734a5e3ed815a65929b1d456258
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-18 10:04:47 -0300

    BACKEND

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+
 //routes
 const authRouter = require("./routes/authRoutes");
-
 //middlewares - travas do meio de projeto e acesso
+
 //config
 
 const dbName = "partytime";
@@ -17,20 +19,20 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //atrelar as rotas ao express
-app.use("/api/auth", authRouter);
+app.use("/api/auth", authRouter)
 
 //conexão mongodb
 mongoose.connect(
 `mongodb://127.0.0.1:27017/${dbName}`, {
 useNewUrlParser : true,
 UseUnifiedTopology : true,
-serverSelectionTimeoutMS : 10000
-}
-)
+serverSelectionTimeoutMS : 20000
+})
+
 app.get("/", (req, res)=> {
-//primeira rota de teste
 res.json({message : "Rota de teste será trocada!!!"});
 });
+
 //escutando a porta
 app.listen(port, ()=>{
 console.log(`O backend está rodando na porta ${port}`)
